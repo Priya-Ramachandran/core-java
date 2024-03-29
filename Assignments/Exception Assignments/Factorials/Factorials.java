1c71fce50693e755c8ee2fff894afcf5288cfc99
Priya-Ramachandran <92845797+Priya-Ramachandran@users.noreply.github.com>
2024-03-29 15:25:25 +0530

    Add files via upload

---
+++ Assignments/Exception Assignments/Factorials/Factorials.java
@@ -0,0 +1,25 @@
+package session;
+
+import java.util.Scanner;
+
+public class Factorials {
+	public static void main(String[] args) throws IllegalArgumentException{
+		String keepGoing = "y";
+		Scanner scan=new Scanner(System.in);
+		
+		
+		while(keepGoing.equals("y") || keepGoing.equals("Y")) {
+			try {
+			System.out.println("Enter an integer: ");
+			int val=scan.nextInt();
+			System.out.println("Factorial(" + val + ")=" + MathUtils.factorial(val));
+			System.out.println("Another factorial? (y/n) ");
+			keepGoing = scan.next();
+		}
+		catch(IllegalArgumentException e) {
+			System.out.println("Exception occured"+e);
+		}
+	}
+		scan.close();
+	}
+}
